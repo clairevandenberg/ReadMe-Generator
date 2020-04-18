5bd312bf2e6191361745548bf4bda3278a41fbcf
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-04-18 15:40:36 +0930

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// At least one badge
+// * Project title
+// * Description
+// * Table of Contents
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
+// * User GitHub profile picture
+// * User GitHub email
+
+// As a <role> I can <capability>, so that <receive benefit>
