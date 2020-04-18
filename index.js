15a920bb14ab4ea6aa3ae320b6c9f969e9ddda2d
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-04-18 16:00:05 +0930

    added js functions for user readme infomation

--- index.js
+++ index.js
@@ -1,14 +1,80 @@
-// At least one badge
-// * Project title
-// * Description
-// * Table of Contents
-// * Installation
-// * Usage
-// * License
-// * Contributing
-// * Tests
-// * Questions
-// * User GitHub profile picture
-// * User GitHub email
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-// As a <role> I can <capability>, so that <receive benefit>
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// getUserInfomation ();
+
+async function userReadMeInfomation() {
+  try {
+    const variables = await inquirer.prompt([
+        {
+        message: "Enter Project Title",
+        name: "ProjectTitle"
+    },
+    {
+        message: "Enter Description",
+        name: "Description"
+    },
+    {
+        message: "Enter Table of Contents",
+        name: "Content"
+    },
+    {
+        message: "Enter Installation",
+        name: "Installation"
+    },
+    {
+        message: "Enter Usage",
+        name: "Usage"
+    },
+    {
+        message: "Enter License",
+        name: "License"
+    },
+    {
+        message: "Enter Contributing",
+        name: "Contributing"
+    },
+    {
+        message: "Enter Tests",
+        name: "Tests"
+    },
+    {
+        message: "Enter Questions",
+        name: "Questions"
+    },
+    {
+        message: "Enter User GitHub profile picture",
+        name: "ProfilePicture"
+    },
+    {
+        message: "Enter User GitHub email",
+        name: "Email"
+    },
+]);
+
+console.log(variables);
+
+// writeFileAsync ('README.md',
+// # ${variables.ProjectTitle}
+// ## ${variables.Description}
+// ## ${variables.Content}
+// ## ${variables.Installation}
+// ## ${variables.Usage}
+// ## ${variables.License}
+// ## ${variables.Contributing}
+// ## ${variables.Tests}
+// ## ${variables.Questions}
+// ## ${variables.ProfilePicture}
+// ## ${variables.Email}
+// `)
+
+ 
+} catch (err) {
+    console.log(err);
+  }
+}
+
+userReadMeInfomation ();
