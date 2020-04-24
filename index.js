076eb22b5217fbbbaefe88056aec044ae86e79c8
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-04-24 12:00:02 +0930

    added type, debugging and added username fucntion

--- index.js
+++ index.js
@@ -4,77 +4,94 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// getUserInfomation ();
 
 async function userReadMeInfomation() {
   try {
     const variables = await inquirer.prompt([
-        {
+    {
         message: "Enter Project Title",
+        type: "input",
         name: "ProjectTitle"
     },
     {
         message: "Enter Description",
+        type: "input",
         name: "Description"
     },
     {
+        message: "Enter Screenshot Link",
+        type: "input",
+        name: "Screenshot"
+    },
+    {
         message: "Enter Table of Contents",
+        type: "input",
         name: "Content"
     },
     {
         message: "Enter Installation",
+        type: "input",
         name: "Installation"
     },
     {
         message: "Enter Usage",
+        type: "input",
         name: "Usage"
     },
     {
         message: "Enter License",
-        name: "License"
+        type: "list",
+        name: "License",
+        choices: [
+            "Public Domain",
+            "Copyleft License",
+            "Permissive License",
+        ]
     },
     {
         message: "Enter Contributing",
+        type: "input",
         name: "Contributing"
     },
     {
         message: "Enter Tests",
+        type: "input",
         name: "Tests"
     },
     {
         message: "Enter Questions",
+        type: "input",
         name: "Questions"
     },
     {
-        message: "Enter User GitHub profile picture",
-        name: "ProfilePicture"
-    },
-    {
-        message: "Enter User GitHub email",
-        name: "Email"
-    },
-]);
+        message: "Enter User GitHub username",
+        type: "input",
+        name: "username",
+        validate: function(answer)
+        { if (answer == "") {
+            return "You Must Enter Your GitHub Username";
+        }
+    return true;
+}
+    
+    }
+])
 
-console.log(variables);
+.then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
 
-// writeFileAsync ('README.md',
-// # ${variables.ProjectTitle}
-// ## ${variables.Description}
-// ## ${variables.Content}
-// ## ${variables.Installation}
-// ## ${variables.Usage}
-// ## ${variables.License}
-// ## ${variables.Contributing}
-// ## ${variables.Tests}
-// ## ${variables.Questions}
-// ## ${variables.ProfilePicture}
-// ## ${variables.Email}
-// `)
+});
 
- 
+await writeFileAsync ("README.md");
+
 } catch (err) {
     console.log(err);
-  }
-}
+  };
+  console.log(variables);
+
+
+console.log("README.MD FIlE CREATED");
+
+};
 
 userReadMeInfomation ();
